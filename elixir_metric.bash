195850d940ae789f6ba1de31b6e574098e9fe1d9
wim-web <45655999+wim-web@users.noreply.github.com>
2024-11-02 20:11:13 +0900

    add prod

--- elixir_metric.bash
+++ elixir_metric.bash
@@ -3,40 +3,35 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # APIエンドポイントのURLを定義
-API_URL="http://localhost:17690/health"
+API_URLS=("http://localhost:17690/health" "http://localhost:17691/health")
 
 # 出力するpromファイルのパス
 OUTPUT_FILE="${SCRIPT_DIR}/elixir_metrics.prom"
 FINAL_OUTPUT_FILE="/var/lib/node_exporter/textfile_collector/elixir_metrics.prom"
 
-# APIからデータを取得
-response=$(curl -s $API_URL)
+# 各エンドポイントから疎通確認し、データを取得してプロメテウスフォーマットに変換
+for API_URL in "${API_URLS[@]}"; do
+    # 疎通確認（タイムアウトは2秒）
+    if curl -s --connect-timeout 2 $API_URL > /dev/null; then
+        # APIからデータを取得
+        response=$(curl -s $API_URL)
 
-# JSONから必要なフィールドを抽出してPrometheusフォーマットに変換
-started_at=$(echo $response | jq -r '.started_at')
-data_frame_version=$(echo $response | jq -r '.data_frame_version')
-order_proposal_version=$(echo $response | jq -r '.order_proposal_version')
-app_version=$(echo $response | jq -r '.app_version')
-status=$(echo $response | jq -r '.status')
-data_frames_consumed=$(echo $response | jq -r '.data_frames_consumed')
-proposals_produced=$(echo $response | jq -r '.proposals_produced')
+        # JSONから必要なフィールドを抽出
+        app_version=$(echo $response | jq -r '.app_version')
+        status=$(echo $response | jq -r '.status')
+        api_url=$(echo $response | jq -r '.["api url"]')
 
-# Prometheusフォーマットでファイルに書き出し
-# echo "# HELP data_frames_consumed The number of data frames consumed." > $OUTPUT_FILE
-# echo "# TYPE data_frames_consumed gauge" >> $OUTPUT_FILE
-# echo "data_frames_consumed $data_frames_consumed" >> $OUTPUT_FILE
+        # Prometheusフォーマットでエンドポイントごとにラベルを付与して書き出し
+        echo "# HELP app_version The version of the application." >> $OUTPUT_FILE
+        echo "# TYPE app_version gauge" >> $OUTPUT_FILE
+        echo "app_version{api_url=\"$api_url\", version=\"$app_version\"} 1" >> $OUTPUT_FILE
 
-# echo "# HELP proposals_produced The number of proposals produced." >> $OUTPUT_FILE
-# echo "# TYPE proposals_produced gauge" >> $OUTPUT_FILE
-# echo "proposals_produced $proposals_produced" >> $OUTPUT_FILE
-
-# 必要に応じて追加のフィールドを記録する
-echo "# HELP app_version The version of the application." >> $OUTPUT_FILE
-echo "# TYPE app_version gauge" >> $OUTPUT_FILE
-echo "app_version{version=\"$app_version\"} 1" >> $OUTPUT_FILE
-
-echo "# HELP status The authorization status of the application." >> $OUTPUT_FILE
-echo "# TYPE status gauge" >> $OUTPUT_FILE
-echo "status{status=\"$status\"} 1" >> $OUTPUT_FILE
+        echo "# HELP status The authorization status of the application." >> $OUTPUT_FILE
+        echo "# TYPE status gauge" >> $OUTPUT_FILE
+        echo "status{api_url=\"$api_url\", status=\"$status\"} 1" >> $OUTPUT_FILE
+    else
+        :
+    fi
+done
 
 mv $OUTPUT_FILE $FINAL_OUTPUT_FILE
